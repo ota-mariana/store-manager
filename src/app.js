50c67a7add1021e01fdd76a77e326276ab76c17a
Mariana <nanomarii10@gmail.com>
2022-11-11 19:48:04 -0300

    [ Mariana Ota ] - Iniciando o Projeto Store Manager

--- src/app.js
+++ src/app.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const productsRoutes = require('./routes/productsRoutes');
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/products', productsRoutes);
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.get('/products', (req, res) => {
+  res.send('testando 1');
 });
 
 // não remova essa exportação, é para o avaliador funcionar
