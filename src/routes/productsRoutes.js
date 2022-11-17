cc9f649d64d060cefe056319af63726717827cb6
Mariana <nanomarii10@gmail.com>
2022-11-17 15:18:08 -0300

    Limpando comentários do arquivo de rotas

--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -3,16 +3,8 @@ const productsController = require('../controllers/productsController');
 
 const routeProducts = express.Router();
 
-// const HTTP_OK_STATUS = 200;
-
 routeProducts.get('/', productsController.getAllProducts);
 
 routeProducts.get('/:id', productsController.getProductsById);
-
-// router.get('/products', async (req, res) => {
-//   const products = req.body;
-//   const [result] = await productsController.insert(products);
-//   res.status(HTTP_OK_STATUS).json(result);
-// });
 
 module.exports = { routeProducts };
